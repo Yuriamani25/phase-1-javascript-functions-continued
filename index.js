be35d5a5037032c0c671f93090a93ff12a4cb5b1
YuriAmani <yuriamani7@gmail.com>
2024-04-04 09:24:11 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(string = "roller-skate") {
+    return (`This Saturday, I want to ${string}!`);
+}
+
+function mondayWork(string = "go to the office") {
+    return (`This Monday, I will ${string}.`)
+}
+
+function wrapAdjective(result){
+    return function name(emphatic = 'a hard worker') {
+        return `You are ${result}${emphatic}${result}!`
+    }
+}
